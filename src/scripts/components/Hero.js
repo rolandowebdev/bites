e83829c1519bd3df6210c22270d5f871d2ee6c1e
RolandoPranataAR <rolandopranata@gmail.com>
2021-12-05 23:53:43 +0700

    fix: fix positioning and layouting

--- src/scripts/components/Hero.js
+++ src/scripts/components/Hero.js
@@ -12,7 +12,7 @@ class Hero extends HTMLElement {
             <source srcset="images/hero-small.jpg" media="(max-width: 600px)">
             <img class="hero-image" src="images/hero-large.jpg" alt="Heroes Image" />
          </picture>
-         <h1 class="hero-title">We Serve The Taste You Love.</h1>
+         <h1 class="hero-title">We Serve The Taste <br> You Love.</h1>
          <p class="hero-description">
             This is a type of restaurant which typically serves food and drinks. in addition to light refreshments such as baked goods or scanks. The term comes the rench word meaning food.
          </p>
